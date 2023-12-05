4d6f335fce801a6848f8848e6732dad44c9b07aa
Adrian Moreno Monterde <amoreno@stemdo.io>
2023-12-05 11:47:53 +0100

    cambios de propiedad realizados al repo

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/visumania/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('visumani  ')
+            node / gitConfigEmail('adrian.moreno.mon@gmail.com')
         }
     }
     triggers {
